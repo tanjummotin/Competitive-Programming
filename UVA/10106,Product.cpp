71caf3b9c36bb793e45d314bbc3067ee2a8b550d
tanjummotin <49992935+tanjummotin@users.noreply.github.com>
2020-09-24 19:53:25 +0600

    Add files via upload

---
+++ UVA/10106,Product.cpp
@@ -0,0 +1,44 @@
+///in the name of ALLAH
+#include<bits/stdc++.h>
+#define mx 600
+using namespace std;
+typedef long long ll;
+int main()
+{
+    string s1,s2;
+    while(cin>>s1)
+    {
+        cin>>s2;
+        reverse(s1.begin(),s1.end());
+        reverse(s2.begin(),s2.end());
+        ll a[mx];
+        for(int i=0;i<mx;i++)
+            a[i]=0;
+        for(int i=0;i<s2.size();i++)
+        {
+            for(int j=0;j<s1.size();j++)
+            {
+                a[i+j]+=(s1[j]-'0')*(s2[i]-'0');
+            }
+        }
+
+        for(int i=0;i<mx;i++)
+        {
+            a[i+1]=a[i+1]+(a[i]/10);
+            a[i]=a[i]%10;
+        }
+        int i;
+        for( i=mx-1;i>0;i--)
+        {
+            if(a[i]!=0)
+                break;
+        }
+
+        for(int j=i;j>=0;j--)
+            cout<<a[j];
+        cout<<endl;
+
+
+    }
+}
+
