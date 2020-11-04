3de371289d0296065002fc9ddf59f590904cc04f
tanjummotin <49992935+tanjummotin@users.noreply.github.com>
2020-11-04 23:04:47 +0600

    Update 1006.Hex-a-bonacci.cpp

--- Light OJ/1006.Hex-a-bonacci.cpp
+++ Light OJ/1006.Hex-a-bonacci.cpp
@@ -2,22 +2,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll a, b, c, d, e, f;
-ll fn( int n ) {
-    if( n == 0 ) return a;
-    if( n == 1 ) return b;
-    if( n == 2 ) return c;
-    if( n == 3 ) return d;
-    if( n == 4 ) return e;
-    if( n == 5 ) return f;
-    return( fn(n-1) + fn(n-2) + fn(n-3) + fn(n-4) + fn(n-5) + fn(n-6) );
-}
+ll a, b, c, d, e, f,n;
 int main() {
-    int n, caseno = 0, cases;
+    int   cases;
     scanf("%d", &cases);
     for(int te=1;te<=cases;te++){
         scanf("%lli %lli %lli %lli %lli %lli %lli", &a, &b, &c, &d, &e, &f, &n);
-        printf("Case %d: %lli\n", te, fn(n) % 10000007);
+        ll dp[10005];
+        if(n==0)dp[0]=a;
+        else if(n==1)dp[1]=b;
+        else if(n==2)dp[2]=c;
+        else if(n==3)dp[3]=d;
+        else if(n==4)dp[4]=e;
+        else if(n==5)dp[5]=f;
+        else
+        {
+            dp[0]=a;dp[1]=b;dp[2]=c;dp[3]=d;dp[4]=e;dp[5]=f;
+            for(int i=6;i<=n;i++)
+            {
+                dp[i]=(dp[i-1]+dp[i-2]+dp[i-3]+dp[i-4]+dp[i-5]+dp[i-6])%10000007;
+                //cout<<dp[i]<<" ";
+            }//cout<<endl;
+        }
+        printf("Case %d: %lli\n", te,dp[n]  % 10000007);
     }
     return 0;
 }
